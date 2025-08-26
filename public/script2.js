f93fe49ade49251c077e3ec7810d7335fa0181c7
edwin <edwinmachucachavez@gmail.com>
2025-08-26 20:12:24 -0300

    tablas

--- public/script2.js
+++ public/script2.js
@@ -2,15 +2,18 @@
 
 new gridjs.Grid({
     columns: [
-        { id: "Iddia", name: "Iddia" },
-        { id: "CodigoDia", name: "CodigoDia" },
-        { id: "DescripcionDia", name: "DescripcionDia" }
+        { id: "PersonaID", name: "PersonaID" },
+        { id: "Apellido", name: "Apellido" },
+        { id: "Nombre", name: "Nombre" },
+        { id: "DNI", name: "DNI" },
+        { id: "Email", name: "Email" },
+        { id: "FechaNacimiento", name: "FechaNacimiento" }
         ],
       
         sort: true,
         server: {
             url: "http://localhost:4000/api/datos",
-            then: data => data.map(post => [post.Iddia, post.CodigoDia, post.DescripcionDia])
+            then: data => data.map(post => [post.PersonaID, post.Apellido, post.Nombre, post.DNI, post.Email, post.FechaNacimiento])
             },
             pagination: true,
             search: true,
